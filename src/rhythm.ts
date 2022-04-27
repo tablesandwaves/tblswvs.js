b40d04f2f7ae4f0f3b246c08dcb9a6c8b70ba530
Steve Meyer <stephen.meyer@wisc.edu>
2022-04-26 21:25:41 -0500

    Add Rhythm transformation

---
+++ src/rhythm.ts
@@ -0,0 +1,62 @@
+import { Melody } from "./melody";
+import { MusicalSymbol } from "./musical_symbol";
+import { Sequence } from "./sequence";
+import { Transformation } from "./transformation";
+
+
+export class Rhythm implements Sequence, Transformation {
+    steps: (1|0)[];
+    length?: number;
+    fillMode: ("wrap"|"silence");
+
+
+    constructor(steps: (1|0)[], fillMode: ("wrap"|"silence") = "wrap", length?: number) {
+        this.steps = steps;
+        this.fillMode = fillMode;
+        this.length = length;
+    }
+
+
+    values(): (1|0)[] {
+        return this.steps;
+    }
+
+
+    applyTo(melody: Melody): Melody {
+        const rhythmHits = this.steps.filter(step => step != 0).length;
+        const stepHits = Math.ceil(melody.steps.length / rhythmHits);
+        let transformedSequence = new Array(stepHits).fill(this.steps).flat();
+
+        if (this.length !== undefined) {
+            if (this.length > transformedSequence.length) {
+                let remainingStepCount = this.length - transformedSequence.length;
+                if (this.fillMode == "silence") {
+                    transformedSequence.push(...new Array(remainingStepCount).fill(0));
+                } else {
+                    let remainingSteps = Math.ceil(remainingStepCount / this.steps.length);
+                    transformedSequence.push(...new Array(remainingSteps).fill(this.steps).flat());
+                }
+            }
+            /*
+            * Truncate the sequence as a final check to make sure it is not longer than the rhythm length
+            * This could happen because this Rhythm's steps are shorter than the transformations's
+            * steps or because the transformedSequence filled out more steps than necessary
+            */
+            transformedSequence = transformedSequence.slice(0, this.length);
+        }
+
+        let processedStepIndex = 0;
+        transformedSequence.forEach((step, i) => {
+            if (step == 1) {
+                transformedSequence[i] = melody.steps[processedStepIndex % melody.steps.length].value;
+                processedStepIndex++;
+            } else {
+                transformedSequence[i] = melody.restSymbol;
+            }
+        });
+
+        let transformedMelody = transformedSequence.map(step => new MusicalSymbol(step));
+
+        return new Melody(transformedMelody, melody.restSymbol, melody.melodicMode);
+    }
+}
---
+++ tests/rhythm_tests.ts
@@ -0,0 +1,70 @@
+import { expect } from "chai"; 
+
+import { Melody, MelodyType } from "../src/melody";
+import { Rhythm } from "../src/rhythm";
+import * as helpers from "./test_helpers";
+
+
+describe("Rhythm", () => {
+    const rhythm = new Rhythm([1, 1, 0]);
+
+    describe("is a Sequence", () => {
+        it("should have steps that can be returned as values", () => expect(rhythm.values()).to.have.ordered.members([1, 1, 0]));
+        it("should have step values of 1 or 0", () => rhythm.steps.forEach(step => expect(step).to.be.a("number").and.to.be.oneOf([0, 1])));
+    });
+
+
+    describe("is a Transformation", () => {
+        const melody = new Melody(helpers.getMelodicSteps([1, 1, 5, 5, 6, 6, 4, 4]));
+
+        describe("when it's step hit length does not have a common denominator with the melody", () => {
+            it("should wrap the melody when ending in a hit", () => {
+                const rhythm   = new Rhythm([1, 1, 0, 0, 1]);
+                const expected = [1, 1, 0, 0, 5, 5, 6, 0, 0, 6, 4, 4, 0, 0, 1];
+                const actual   = rhythm.applyTo(melody).values();
+                expect(actual).to.have.ordered.members(expected);
+            });
+
+            it("should wrap the sequence when ending in a rest", () => {
+                const rhythm = new Rhythm([1, 1, 0, 1, 0]);
+                const expected = [1, 1, 0, 5, 0, 5, 6, 0, 6, 0, 4, 4, 0, 1, 0];
+                const actual = rhythm.applyTo(melody).values();
+                expect(actual).to.have.ordered.members(expected);
+            });
+        });
+
+
+        describe("when it's step hit length has a common denominator with the melody", () => {
+            it("should wrap the rhythm when ending in a hit", () => {
+                const rhythm = new Rhythm([1, 0, 1]);
+                const expected = [1, 0, 1, 5, 0, 5, 6, 0, 6, 4, 0, 4];
+                const actual = rhythm.applyTo(melody).values();
+                expect(actual).to.have.ordered.members(expected);
+            });
+
+            it("should wrap the rhythm when ending in a rest", () => {
+                const rhythm = new Rhythm([1, 1, 0]);
+                const expected = [1, 1, 0, 5, 5, 0, 6, 6, 0, 4, 4, 0];
+                const actual = rhythm.applyTo(melody).values();
+                expect(actual).to.have.ordered.members(expected);
+            });
+        });
+
+
+        describe("when it has a bar length", () => {
+            it("repeats sequence to fill out the bar if set to wrap", () => {
+                const rhythm = new Rhythm([1, 1, 0], "wrap", 16);
+                const expected = [1, 1, 0, 5, 5, 0, 6, 6, 0, 4, 4, 0, 1, 1, 0, 5]
+                const actual = rhythm.applyTo(melody).values();
+                expect(actual).to.have.ordered.members(expected);
+            });
+
+            it("will guarantee a step length regardless of the sequence's step size by adding rests", () => {
+                const rhythm = new Rhythm([1], "silence", 16);
+                const expected = [1, 1, 5, 5, 6, 6, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0];
+                const actual = rhythm.applyTo(melody).values();
+                expect(actual).to.have.ordered.members(expected);
+            });
+        });
+    });
+});
