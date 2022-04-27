1e8bce159f98d0d89995983267ff915c767ebe7b
Steve Meyer <stephen.meyer@wisc.edu>
2022-04-26 23:24:39 -0500

    Update module exports for Rhythm

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 // Note: using an import will still build, but Max's node processor cannot see the properties.
 // Using require() instead.
-const sequence = require("./sequence");
 const melody   = require("./melody");
 const vector   = require("./melodic_vector");
+const rhythm   = require("./rhythm");
 
 
 module.exports.MelodyType    = melody.MelodyType;
 module.exports.Melody        = melody.Melody;
 module.exports.MelodicVector = vector.MelodicVector;
+module.exports.Rhythm        = rhythm.Rhythm;
