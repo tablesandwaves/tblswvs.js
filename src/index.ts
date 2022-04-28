a99a5f7e1d7deede1cee8f334d0e5dd139ac2425
Steve Meyer <stephen.meyer@wisc.edu>
2022-04-27 22:14:28 -0500

    Export LindenmayerSystem with the module

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 // Note: using an import will still build, but Max's node processor cannot see the properties.
 // Using require() instead.
-const melody   = require("./melody");
-const vector   = require("./melodic_vector");
-const rhythm   = require("./rhythm");
+const melody  = require("./melody");
+const vector  = require("./melodic_vector");
+const rhythm  = require("./rhythm");
+const lsystem = require("./lindenmayer_system");
 
-
-module.exports.MelodyType    = melody.MelodyType;
-module.exports.Melody        = melody.Melody;
-module.exports.MelodicVector = vector.MelodicVector;
-module.exports.Rhythm        = rhythm.Rhythm;
+module.exports.MelodyType        = melody.MelodyType;
+module.exports.Melody            = melody.Melody;
+module.exports.MelodicVector     = vector.MelodicVector;
+module.exports.Rhythm            = rhythm.Rhythm;
+module.exports.LindenmayerSystem = lsystem.LindenmayerSystem;
