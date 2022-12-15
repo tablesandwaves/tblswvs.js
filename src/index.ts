4a0e5f41615074571302a15cbd265aa8691ce08b
Steve Meyer <stephen.meyer@wisc.edu>
2022-12-14 21:27:22 -0600

    Bug fix: export the Harmony class

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // Note: using an import will still build, but Max's node processor cannot see the properties.
 // Using require() instead.
 const melody  = require("./melody");
+const harmony = require("./harmony");
 const vector  = require("./melodic_vector");
 const rhythm  = require("./rhythm");
 const lsystem = require("./lindenmayer_system");
@@ -8,6 +9,7 @@ const markov  = require("./markov_chain");
 
 module.exports.MelodyType        = melody.MelodyType;
 module.exports.Melody            = melody.Melody;
+module.exports.Harmony           = harmony.Harmony;
 module.exports.MelodicVector     = vector.MelodicVector;
 module.exports.Rhythm            = rhythm.Rhythm;
 module.exports.LindenmayerSystem = lsystem.LindenmayerSystem;
