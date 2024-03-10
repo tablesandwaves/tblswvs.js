9eee1a0ef113bef3d4f8eb4184d7d68f5f23810f
Steve Meyer <stephen.meyer@wisc.edu>
2024-03-10 15:38:25 -0500

    Push a new bit into the shift register

--- src/shift_register.ts
+++ src/shift_register.ts
@@ -1,3 +1,6 @@
+import { rotate } from "./helpers";
+
+
 export class ShiftRegister {
     bits: (0|1)[];
     length: number;
@@ -6,5 +9,11 @@ export class ShiftRegister {
     constructor(length: number = 8) {
         this.length = length;
         this.bits = new Array(length).fill(0);
+    }
+
+
+    push(bit: (0|1)) {
+        this.bits = rotate(this.bits, 1);
+        this.bits[0] = bit;
     }
 }
--- tests/shift_register_tests.ts
+++ tests/shift_register_tests.ts
@@ -6,7 +6,20 @@ import { ShiftRegister } from "../src/shift_register";
 describe("ShiftRegister", () => {
     describe("when created", () => {
         const shiftRegister = new ShiftRegister();
+
         it("has a bit array of zeroes", () => expect(shiftRegister.bits).to.have.ordered.members([0, 0, 0, 0,  0, 0, 0, 0]));
         it("has a default length of 8", () => expect(shiftRegister.length).to.eq(8));
+        it("can have its bit array set", () => {
+            shiftRegister.bits = [1, 1, 1, 1,  0, 0, 0, 0];
+            expect(shiftRegister.bits).to.have.ordered.members([1, 1, 1, 1,  0, 0, 0, 0]);
+        });
+
+        describe("pushing new bits into it", () => {
+            it("adds the new bit to the beginning and rotates all bits to the right", () => {
+                shiftRegister.bits = [1, 0, 1, 0,  1, 0, 1, 0];
+                shiftRegister.push(1);
+                expect(shiftRegister.bits).to.have.ordered.members([1, 1, 0, 1,  0, 1, 0, 1]);
+            });
+        });
     });
 });
